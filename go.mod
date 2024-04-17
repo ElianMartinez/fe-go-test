8fe3f84bea52daf0bb9b24dbdc15987a2fc4bcd2
ElianMartinez <elianmartinez157@gmail.com>
2024-04-16 21:21:20 -0400

    Refactor code for improved performance and readability

---
+++ go.mod
@@ -0,0 +1,3 @@
+module fe-go-test
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("OS\t\t", runtime.GOOS)
+	fmt.Println("ARCH\t\t", runtime.GOARCH)
+	fmt.Println("CPUs\t\t", runtime.NumCPU())
+	fmt.Println("GoRoutines\t", runtime.NumGoroutine())
+	fmt.Println("GoMaxProcs\t", runtime.GOMAXPROCS(0))
+	fmt.Println("Version\t\t", runtime.Version())
+
+}
